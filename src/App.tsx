24bbc3ad1c054bf50828a931218be94eaf520d4a
arifulislam124 <arifulislamapple.info@gmail.com>
2026-09-13 12:32:23 +0600

    Bannar Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import Bannar from './Bannar'
 import NavBar from './NavBar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <NavBar/>
+      <Bannar/>
     
     </>
   )
